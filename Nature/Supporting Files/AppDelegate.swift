bfdb58a3e3da96764b4fba88d7644ed101203061
Oliver Kulpakko <oliver@kulpakko.com>
2018-04-18 15:20:19 +0300

    Embedded CategoriesViewController inside a navigation controller.

--- Nature/Supporting Files/AppDelegate.swift
+++ Nature/Supporting Files/AppDelegate.swift
@@ -25,7 +25,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         let viewController = CategoriesViewController()
         
-        window!.rootViewController = viewController
+        let navigationController = UINavigationController(rootViewController: viewController)
+        
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
 
