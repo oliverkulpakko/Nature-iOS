2daa7b36d76735594d4d4e76ff23e87546727286
Oliver Kulpakko <oliver@kulpakko.com>
2018-04-21 11:59:51 +0300

    Added search to items.

--- Nature/Views/Items/ItemsViewController.swift
+++ Nature/Views/Items/ItemsViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 import Imaginary
 
-class ItemsViewController: BaseViewController, UITableViewDelegate, UITableViewDataSource {
+class ItemsViewController: BaseViewController, UITableViewDelegate, UITableViewDataSource, UISearchResultsUpdating {
 
 	//MARK: View Lifecycle
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+
+		searchController.searchResultsUpdater = self
+		searchController.searchBar.placeholder = "items.search.placeholder".localized
+		definesPresentationContext = true
+		
+		if #available(iOS 11.0, *) {
+			navigationItem.searchController = searchController
+			navigationItem.hidesSearchBarWhenScrolling = false
+			searchController.obscuresBackgroundDuringPresentation = false
+		} else {
+			tableView.tableHeaderView = searchController.searchBar
+		}
 	}
 	
 	// MARK: BaseViewController
@@ -40,8 +52,15 @@ class ItemsViewController: BaseViewController, UITableViewDelegate, UITableViewD
 	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		tableView.deselectRow(at: indexPath, animated: true)
 		
+		let item: Item
+		if isSearching {
+			item = filteredItems[indexPath.row]
+		} else {
+			item = category.items[indexPath.row]
+		}
+		
 		let itemViewController = ItemViewController()
-		itemViewController.item = category.items[indexPath.row]
+		itemViewController.item = item
 		
 		navigationController?.pushViewController(itemViewController, animated: true)
 	}
@@ -49,6 +68,10 @@ class ItemsViewController: BaseViewController, UITableViewDelegate, UITableViewD
 	// MARK: UITableViewDataSource
 	
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		if isSearching {
+			return filteredItems.count
+		}
+		
 		return category.items.count
 	}
 	
@@ -57,7 +80,12 @@ class ItemsViewController: BaseViewController, UITableViewDelegate, UITableViewD
 			return UITableViewCell()
 		}
 		
-		let item = category.items[indexPath.row]
+		let item: Item
+		if isSearching {
+			item = filteredItems[indexPath.row]
+		} else {
+			item = category.items[indexPath.row]
+		}
 		
 		cell.titleLabel.text = item.title
 		cell.subtitleLabel.text = item.subtitle
@@ -69,10 +97,37 @@ class ItemsViewController: BaseViewController, UITableViewDelegate, UITableViewD
 		return cell
 	}
 	
+	// MARK: UISearchResultsUpdating
+	
+	func updateSearchResults(for searchController: UISearchController) {
+		filterItems(for: searchController.searchBar.text ?? "")
+	}
+	
 	// MARK: Instance Functions
+	
+	func filterItems(for text: String) {
+		filteredItems = category.items.filter({ item -> Bool in
+			return item.title.lowercased().contains(text.lowercased())
+		})
+		
+		tableView.reloadData()
+	}
+	
+	// MARK: Calculated Properties
+	
+	var isSearchBarEmpty: Bool {
+		return (searchController.searchBar.text?.isEmpty ?? true)
+	}
+	
+	var isSearching: Bool {
+		return searchController.isActive && !isSearchBarEmpty
+	}
 	
 	// MARK: Instance Variables
 	
+	var searchController = UISearchController(searchResultsController: nil)
+	
+	var filteredItems = [Item]()
 	var category: Category!
 	
 	// MARK: IBOutlets
