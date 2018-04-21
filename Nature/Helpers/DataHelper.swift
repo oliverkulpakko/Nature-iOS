cbfd521f44d283ee83e717b70ba76d183ce53d7f
Oliver Kulpakko <oliver@kulpakko.com>
2018-04-21 10:17:19 +0300

    Country can now be empty in DataHelper when requesting categories.

--- Nature/Helpers/DataHelper.swift
+++ Nature/Helpers/DataHelper.swift
@@ -9,7 +9,13 @@
 import Foundation
 
 class DataHelper {
-	class func fetchCategories(for country: String, completion: @escaping (_ result: [Category]?, Error?) -> Void) {
+	
+	/// Fetch categories for a specified country, or if an empty country is given, for all countries.
+	/// The response can be very big, especially when fetching for all countries.
+	///
+	/// - Parameters:
+	///   - completion: An optional array of Category, and an optional error.
+	class func fetchCategories(for country: String = "", completion: @escaping (_ result: [Category]?, Error?) -> Void) {
 		guard let url = URL(string: "https://eaststudios.net/api/Nature/category/?country=" + country) else {
 			completion(nil, nil)
 			return
